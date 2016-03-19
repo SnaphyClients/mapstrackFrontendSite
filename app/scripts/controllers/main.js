e00a09cf41d9ea2a1f28eab3065abfc1097b9a37
robinskumar73 <robinskumar73@gmail.com>
2016-03-20 04:43:06 +0530

    Search functionalities adde

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,30 @@
  * Controller of the mapstrackFrontEndApp
  */
 angular.module('mapstrackFrontEndApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', ["$scope", "$window", function ($scope, $window) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    /**
+     * Get the tracking code for the data..
+     * @param trackingCode
+       */
+    $scope.trackData = function(trackingCode){
+      console.log(trackingCode);
+      if(!trackingCode){
+        return;
+      }
+
+      if(trackingCode.trim() !== ""){
+        //Now redirect to another page..
+        $window.location.href = 'track/' + trackingCode;
+      }
+
+    };
+
+
+
+  }]);
