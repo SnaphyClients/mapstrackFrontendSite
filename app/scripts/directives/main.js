bd7e6c0c581ae59cbbb4bbb3c71582c4abf602ee
robinskumar73 <robinskumar73@gmail.com>
2016-03-20 04:05:00 +0530

    First commit

---
+++ app/scripts/directives/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mapstrackFrontEndApp
+ * @description
+ * # mapstrackFrontEndApp
+ *
+ * Directive of the application.
+ */
+angular.module('mapstrackFrontEndApp')
+
+
