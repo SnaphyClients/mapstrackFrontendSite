bba17b2780213517b1fea9811e24aa60ef13f55f
robinskumar73 <robinskumar73@gmail.com>
2016-03-20 03:58:56 +0530

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mapstrackFrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mapstrackFrontEndApp
+ */
+angular.module('mapstrackFrontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
